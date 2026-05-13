f6816a6a584b3b9eb82fb1cc66c7cbffe15cd8c0
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-13 21:13:18 +0300

    feat: add GameManager script

---
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,49 @@
+using ColorBlockJam.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public GameState State { get; private set; } = GameState.Boot;
+
+    [SerializeField] private string _gameplaySceneName = "Gameplay";
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        ServiceLocator.Register(this);
+
+        SceneManager.LoadScene(_gameplaySceneName, LoadSceneMode.Single);
+
+        Debug.Log("[GameManager] Bootstrap complete.");
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance != this)
+            return;
+
+        ServiceLocator.Clear();
+        Instance = null;
+    }
+
+    public void SetState(GameState newState)
+    {
+        if (State == newState)
+            return;
+
+        Debug.Log($"[GameManager] State changed to: {State} -> {newState}");
+
+        State = newState;
+    }
+}
