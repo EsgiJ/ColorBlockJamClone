906cea4a5a5912e1ec07fd0b2160647ae11a9449
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-13 21:07:38 +0300

    feat: add GameState script

---
+++ Assets/_Project/Scripts/Core/GameState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorBlockJam.Core
+{
+    public enum GameState
+    {
+        Bootstrap,
+        Start,
+        Gameplay,
+        Complete,
+        GameOver
+    }
+}
