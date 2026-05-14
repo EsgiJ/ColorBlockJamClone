7b57734038e0b6d0b03183f8a734094a6bd496f2
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-14 13:36:37 +0300

    feat: create LevelDataSO

---
+++ Assets/_Project/Scripts/Data/LevelDataSO.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Data;
+using UnityEngine;
+
+namespace ColorBlockJamClone.Data
+{
+    [Serializable]
+    public struct BlockPlacement
+    {
+        public BlockShapeSO shape;
+        public BlockColor color;
+        public Vector2Int gridPosition;     // origin of the cell
+        [Range(0, 3)] public int rotationSteps;    
+    }
+
+    [Serializable]
+    public struct GatePlacement
+    {
+        public BlockColor color;
+        public GridSide side;
+        public int positionAlongSide;
+        [Range(1, 4)] public int width;
+    }
+
+    [CreateAssetMenu(fileName = "Level_", menuName = "ColorBlockJam/Level Data")]
+    public class LevelDataSO : ScriptableObject
+    {
+        [Header("Grid")]
+        public Vector2Int gridSize = new(7,8);
+        public Vector2Int[] blockedCells;
+
+        [Header("Time")]
+        [Min(5f)] public float time = 120f;
+
+        [Header("Blocks")]
+        public BlockPlacement[] blocks;
+
+        [Header("Gates")]
+        public GatePlacement[] gates;
+    }
+}
